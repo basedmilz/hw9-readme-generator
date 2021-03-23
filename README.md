0492823618c1816fc5ca6c249a888bf9f2dd89d6
basedmilz <basedmilz@gmail.com>
2021-03-23 18:58:32 -0400

    added function to write readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# hw9-readme-generator
+# First
+
--- index.js
+++ index.js
@@ -1,65 +1,74 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generate = require('./utils/generateMarkdown')
+const generate = require('./utils/generateMarkdown');
+const Choices = require('inquirer/lib/objects/choices');
 
 
 // TODO: Create an array of questions for user input
-inquirer.prompt([ 
-    {
-const: questions = [{
-    type: 'input',
-    message:'What is your project Title?',
-    name: 'title'
-    
-},
-{
-    type: 'input',
-    message:'Brief Description of your Project',
-    name: 'description'
-    
-},
-{
-    type: 'input',
-    message:'What are your installation instructions?',
-    name: 'install'
-    
-},
-{
-    type: 'input',
-    message:'List and outside collaborators or contributions',
-    name: 'credits'
-    
-},
-{
-    type: 'list',
-    message:'What licenses are used in production?',
-    name: 'license'
-    
-},
-{
-    type: 'input',
-    message:'Reference you github username here',
-    name: 'github'
-    
-},
-{
-    type: 'input',
-    message:'What is associated email?',
-    name: 'email'
-    
-},
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is your project Title?',
+            name: 'title'
+
+        },
+        {
+            type: 'input',
+            message: 'Brief Description of your Project',
+            name: 'description'
+
+        },
+        {
+            type: 'input',
+            message: 'What are your installation instructions?',
+            name: 'install'
+
+        },
+        {
+            type: 'input',
+            message: 'List any outside collaborators or contributions',
+            name: 'credits'
+
+        },
+        {
+            type: 'list',
+            message: 'What licenses are used in production?',
+            name: 'license',
+            choices: ['BSD',
+            'MIT',
+            'GPL',]
+
+        },
+        {
+            type: 'input',
+            message: 'Reference you github username here',
+            name: 'github'
+
+        },
+        {
+            type: 'input',
+            message: 'What is associated email?',
+            name: 'email'
+
+        },
 
 
-],
-    }])
+    ],
+    )
+    .then((data) => {
+       fs.writeFile('README.md',generate(data), (err) =>
+          err ? console.log(err) : console.log('Successfully created index.html!')
+        );
+      });
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
 function init() {
 
-}
+        }
 
 // Function call to initialize app
 init();
